7c6cbf492f5813c95c10e303bd4b9d49f500b2f0
mo30qari <mo30qari@gmail.com>
2021-02-07 16:14:43 +0330

    Repaired <validateObj> method

--- obj/Validate.js
+++ obj/Validate.js
@@ -1,7 +1,7 @@
 const format = require("../config/request.json")
 const Validate = function () {
 
-    this.validateType = function (type) {//Every request has a __Type field that shows type of the request. This is always string...
+    this.validateType = function (type) {//Every request has a __Type property that shows type of the request. This is always string...
         let result = { status: true, errors: [] }
 
         type = type.trim()
@@ -9,7 +9,7 @@ const Validate = function () {
         if (!type && type === "") {
             result.error = "The __Type does not exist"
         } else if (typeof type != "string") {
-            result.errors.push("The field must be string")
+            result.errors.push("The property must be string")
         } else if (type.length < 3) {
             result.errors.push("The __Type is too short")
         } else if (type.length > 32) {
@@ -30,7 +30,7 @@ const Validate = function () {
         str = str.trim()
 
         if (typeof str != "string") {
-            result.errors.push("The field must be string")
+            result.errors.push("The property must be string")
         } else if (!str.match(regex)) {
             result.errors.push("The string structure isn't valid")
         } else if (str.length < min) {
@@ -50,7 +50,7 @@ const Validate = function () {
         let result = { status: true, errors: [] }
 
         if (typeof num != "number") {
-            result.errors.push("The field must be number")
+            result.errors.push("The property must be number")
         } else if (num < min) {
             result.errors.push("The number is too low")
         } else if (num > max) {
@@ -68,7 +68,7 @@ const Validate = function () {
         let result = { status: true, errors: [] }
 
         if (typeof bool != "boolean") {
-            result.errors.push("The field must be boolean")
+            result.errors.push("The property must be boolean")
         }
 
         if (result.errors.length) {
@@ -78,11 +78,17 @@ const Validate = function () {
         return result
     }
 
-    this.validateObject = function (obj) {//This is a too silly method to validate an object. It should be better in the near future. 
+    this.validateObject = function (obj, props) {//This is a too silly method to validate an object. It should be better in the near future. 
         let result = { status: true, errors: [] }
 
         if (typeof obj != "object") {
-            result.errors.push("The field must be object")
+            result.errors.push("The property must be object")
+        } else {
+            props.forEach(function (key) {
+                if (!obj.hasOwnProperty(key)) {
+                    result.errors.push(key + ": " + "doesn't exists in property")
+                }
+            })
         }
 
         if (result.errors.length) {
@@ -98,22 +104,22 @@ const Validate = function () {
         let type = this.validateType(req.__Type)
 
         if (type.status) {//__Type is valid
-            let f = format[req.__Type] //request.json
+            let structure = format[req.__Type] //request.json
             let r //Template for saving methods returns
 
             for (const [key, value] of Object.entries(req)) {//Iterating in request
 
                 if (key != "__Type") {//The __Type is already checked.
 
-                    switch (f[key].type) {
+                    switch (structure[key].type) {
                         case "string":
-                            r = this.validateString(value, f[key].min, f[key].max)
+                            r = this.validateString(value, structure[key].min, structure[key].max)
                             if (!r.status) {
                                 result.errors.push(key + ": " + r.errors)
                             }
                             break
                         case "number":
-                            r = this.validateNumber(value, f[key].min, f[key].max)
+                            r = this.validateNumber(value, structure[key].min, structure[key].max)
                             if (!r.status) {
                                 result.errors.push(key + ": " + r.errors)
                             }
@@ -125,13 +131,13 @@ const Validate = function () {
                             }
                             break
                         case "object":
-                            r = this.validateObject(value)
+                            r = this.validateObject(value, structure[key].properties)
                             if (!r.status) {
                                 result.errors.push(key + ": " + r.errors)
                             }
                             break
                         default:
-                            result.errors.push("The type of field doesn't exists")
+                            result.errors.push("The type of property doesn't exist!")
                     }
 
                 }
