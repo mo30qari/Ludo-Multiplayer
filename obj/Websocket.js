351461dbc722709733c91383d9dea1d7cf08a12a
mo30qari <mo30qari@gmail.com>
2021-02-07 16:15:46 +0330

    Added <handleCreateRoomReq>

--- obj/Websocket.js
+++ obj/Websocket.js
@@ -26,6 +26,9 @@ const Websocket = function (ws) {
                 case "InitialReq": //Registering player in the Websocket server 
                     this.handleInitialReq()
                     break
+                case "CreateRoomReq": //When player creates a room
+                    this.handleCreateRoomReq()
+                    break
             }
 
         } else {//Invalid request
@@ -33,21 +36,43 @@ const Websocket = function (ws) {
         }
     }
 
+    //HANDLE FUNCTIONS
+
     this.handleInitialReq = function () {
         let player = new Player(this.message.PlayerID)
         let result = player.getStatus()
 
         if (result.status) {//The player exists and isn't deleted
             player.setWS(ws)//From now on the player can be verified with WS
+            this.sendInitialRes(result.player.name)
         } else {
             this.showErrors(DEFAULT_ERROR_TITLE, result.errors)
         }
     }
 
+    this.handleCreateRoomReq = function () {
+        
+    }
+
+    //End of HANDLE FUNCTIONS
+
+    //SEND RESPONSE FUNCTIONS
+
+    this.sendInitialRes = function (name) {
+        ws.send(JSON.stringify({
+            __Type: "InitialRes",
+            Player: {
+                Name: name
+            }
+        }))
+    }
+
+    //End of SEND RESPONSE FUNCTIONS
+
     this.close = function () {
         console.log("Websocket closed!")
     }
-    
+
     this.showErrors = function (status, errors) {
         ws.send(JSON.stringify({
             Status: status,
