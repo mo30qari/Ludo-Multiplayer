994b70025bbaa613b446c0615bbdcda66c00c663
mo30qari <mo30qari@gmail.com>
2021-02-08 17:47:59 +0330

    Added fetch player on start & update methods

--- obj/Player.js
+++ obj/Player.js
@@ -1,31 +1,61 @@
-const DB = require("./Database").Database
-let db = new DB()
+const Database = require("./Database").Database
+let db = new Database()
+let onlinePlayers = []
 
-const Player = function (id) {
+const Player = function (ws, id = undefined, newUser = 0) {
 
+    this.ws = ws
     this.id = id
+
+    if (id && newUser) {
+        db.insertPlayer(this)
+        
+    } else if (id && !newUser) {
+        let result = db.getPlayerByID(this.id)
+
+        if (result.status) {
+            this.name = result.player.name
+        } else {
+            return result
+        }
+    }
+    else if (ws) {
+        let result = db.getPlayerByWS(this.ws)
+
+        if (result.status) {
+            this.name = result.player.name
+            this.id = result.player.id
+        } else {
+            return result
+        }
+    }
+
+    // SET FUNCTIONS
 
     this.setName = function (name) {
         this.name = name
+        db.updatePlayer(this, "name", name)
     }
 
-    this.addToPlayers = function () {
-        db.insertPlayer(this)
-    }
-
-    this.getStatus = function () {//Shows a player is active and true or not
-        return db.getPlayer(this)
-    }
-    
     this.setWS = function (ws) {
-        let result = db.updatePlayer(this, "ws", ws)
-        
-        if (!result.status) {
-            console.log(result.errors)//Unexpected error! I suppose everything is OK here!
+        this.ws = ws
+        db.updatePlayer(this, "ws", this.ws)// <ws> was got when the instance was created
+        onlinePlayers.push(this)// The online players should be registered in <onlinePlayers> to fast access
+    }
+
+    // End of SET FUNCTIONS
+
+    // GET FUNCTIONS
+
+    this.getMe = function () {//Shows a player is active and true or not
+        if (onlinePlayers.includes(this)) {
+            return { player: this }//Standard DB format
         } else {
-            this.ws = ws
+            return db.getPlayerByID(this.id)
         }
     }
+
+    // End of GET FUNCTIONS
 
 }
 
