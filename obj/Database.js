fa3658ba0ba01adc4e51051b7ccd0c9a3f34aecc
mo30qari <mo30qari@gmail.com>
2021-02-08 17:46:15 +0330

    Divided <getPlayer> to <getPlayerByID> & <getPlayerByWS>

--- obj/Database.js
+++ obj/Database.js
@@ -7,9 +7,28 @@ const Database = function (){
         this.players.push(player)
     }
     
-    this.getPlayer = function (ply) {
+    this.getPlayerByID = function (id) {
         let result = { status: true, errors: [] }
-        let player = this.players.find(e => e.id === ply.id)
+        let player = this.players.find(e => e.id === id)
+        
+        if (!player) {
+            result.errors.push("The player doesn't exist")
+        } else if (player.deleted) {
+            result.errors.push("The player was deleted")
+        }
+        
+        if (result.errors.length) {
+            result.status = false
+        } else {
+            result.player = player
+        }
+
+        return result
+    }
+    
+    this.getPlayerByWS = function (ws) {
+        let result = { status: true, errors: [] }
+        let player = this.players.find(e => e.ws === ws)
         
         if (!player) {
             result.errors.push("The player doesn't exist")
